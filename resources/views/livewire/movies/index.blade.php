9fb561096d3467700f700860e82aa2ec347d3c90
natchapolsr-ops <natchapol.sr@kkumail.com>
2025-10-03 23:20:55 +0700

    Update index.blade.php

--- resources/views/livewire/movies/index.blade.php
+++ resources/views/livewire/movies/index.blade.php
@@ -8,8 +8,8 @@
                     ? $movie->poster_url
                     : asset($movie->poster_url ?? 'https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABfgDSo3bKLA39qLUxd1bbr9YMMMCrpAfdTuwljzkLKvCmRTXC5yAWrmezCVzc3HTmAsoVaUbyUKCxARsOb_rMEjmx1RacSbQ0M-X.jpg?r=33c');
             @endphp
-            <a href="{{ route('movies.show',$movie) }}" class="bg-neutral-800 rounded-lg overflow-hidden hover:ring-2 ring-amber-500 transition">
-                <img src="{{ $poster }}" alt="{{ $movie->title }}" class="w-full h-56 object-cover">
+            <a href="{{ route('movies.show',$movie) }}" class="bg-neutral-800 rounded-lg overflow-hidden hover:ring-2 ring-amber-500 transition flex flex-col items-center">
+                <img src="{{ $poster }}" alt="{{ $movie->title }}" class="h-28 max-w-[100px] object-cover mt-4 mb-2 rounded shadow">
                 <div class="p-3">
                     <div class="font-semibold">{{ $movie->title }}</div>
                     <div class="text-xs text-neutral-400">{{ strtoupper($movie->genre) }}</div>
